fff0807f6e708740254f51a100e8c1be0fc73e02
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 14:57:58 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  turn_counter  = 0
+  board.each do |position|
+    if position != " "
+      turn_counter += 1
+    end
+  end
+turn_counter
+end
+
+def current_player(board)
+
+  if turn_count(board).even?
+    turn = "X"
+  else
+    turn = "O"
+  end
+turn
+end
